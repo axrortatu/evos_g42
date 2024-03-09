6a39eedc0c6fe998854bd2ba10ce89c13e4298f7
Xadje <xojiakbarturgunov273@gmail.com>
2024-03-09 21:22:41 +0500

    improved index to indexCategories

--- src/jahongir/service/CategoryService.java
+++ src/jahongir/service/CategoryService.java
@@ -5,7 +5,7 @@ import sardor.model.Category;
 public class CategoryService {
     private Category[] categories = new Category[100];
 
-    private int index;
+    private int indexCategories;
 
     public Category[] getCategories() {
         return categories;
