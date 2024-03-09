61e759027a71d82e40e91f890781503f905a4196
Azamjon <macbookpro@MacBook-Pro-MacBook.local>
2024-03-09 21:13:43 +0500

    jahongirga delete va add qo'shildi

--- src/jahongir/User.java
+++ src/jahongir/User.java
@@ -1,2 +1,17 @@
-package jahongir;public class User {
+package jahongir;
+
+import sardor.model.BaseModel;
+
+import java.util.UUID;
+
+public class User extends BaseModel {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
