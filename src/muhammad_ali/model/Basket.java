e417a6c4a9a7a7786a725f3276cbb9636c36ea1c
Muhammadali <akbarovmuhammadali777@gmail.com>
2024-03-10 16:41:19 +0500

    addBasket and getMyBasket has been added

--- src/muhammad_ali/model/Basket.java
+++ src/muhammad_ali/model/Basket.java
@@ -5,12 +5,23 @@ import java.util.UUID;
 public class Basket extends BaseModel {
     private UUID productId;
     private int cnt;
-    private boolean active;
+    private boolean active = true;
     private double deliveryPrice;
     private UUID userId;
 
+    public Basket(UUID userId, UUID productId, int cnt, int deliveryPrice){
+        this.userId = userId;
+        this.productId = productId;
+        this.cnt = cnt;
+        this.deliveryPrice = deliveryPrice;
+    }
+
     public double getDeliveryPrice() {
         return deliveryPrice;
+    }
+
+    public boolean isActive() {
+        return active;
     }
 
     public void setActive(boolean active) {
