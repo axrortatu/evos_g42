e3866650c4d65511676f39cc2e74613c9f5f4fd3
akmal <akmalnajimov70@gmail.com>
2024-03-11 17:58:29 +0500

    Add and validate methods completed, check and Write comment

--- src/jahongir/service/CategoryService.java
+++ src/jahongir/service/CategoryService.java
@@ -15,43 +15,58 @@ public class CategoryService extends BaseService {
     @Override
     public boolean add(Object object) {
         Category category = (Category) object;
-
-        if (validate(category)) {}
-
-        return false;
+        if (validate(category)) {
+            categories[indexCategories++] = category;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean delete(UUID id) {
+
         return false;
     }
 
     @Override
     public boolean update(UUID id, Object object) {
         Category updateCategory = (Category) object;
-
         for (int i = 0; i < indexCategories; i++) {
             if (categories[i].getId().equals(id)) {
                 categories[i].setName(updateCategory.getName());
                 return true;
             }
         }
-
         return false;
     }
 
     @Override
     public Object[] list(UUID id) {
-        Category[] parentCategories = new Category[100];
-        int index = 0;
-
-        return parentCategories;
+        Category[] parentCategories = new Category[indexCategories];
+        int count = 0;
+        for (int i = 0; i < indexCategories; i++) {
+            if (categories[i].getId() != null && categories[i].getId().equals(id)) {
+                parentCategories[count++] = categories[i];
+            }
+        }
+        Category[] result = new Category[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = parentCategories[i];
+        }
+        return result;
     }
 
     private boolean validate(Category category) {
-
-        return false;
+        if (category == null) {
+            return false;
+        }
+        if (category.getName() == null || category.getName().isEmpty()) {
+            return false;
+        }
+        return true;
     }
 
 }
+
 
