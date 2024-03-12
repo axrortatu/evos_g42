07f4d85ad0cc7c34c7ea9cf159e76f6d7ae8b37f
Sardorbek <berdiqolovmuzaffar@gmail.com>
2024-03-12 15:00:03 +0500

    user class yozildi

--- src/sardor/service/UserService.java
+++ src/sardor/service/UserService.java
@@ -1,10 +1,11 @@
 package sardor.service;
 
+import sardor.model.Product;
 import sardor.model.User;
 
 import java.util.UUID;
 
-public class UserService implements InterfaceService{
+public class UserService implements InterfaceService {
     static User[] users = new User[1000];
     static int userCount = 0;
 
@@ -12,24 +13,25 @@ public class UserService implements InterfaceService{
     @Override
     public boolean add(Object object) {
         User user = (User) object;
-        if (!hasUser(user.getUsername())){
+        if (!hasUser(user.getUsername())) {
             users[userCount++] = user;
             return true;
         }
         return false;
     }
 
-    private boolean hasUser(String username){
-        for (User user : users){
-            if (user != null && user.getUsername().equals(username)){
+    private boolean hasUser(String username) {
+        for (User user : users) {
+            if (user != null && user.getUsername().equals(username)) {
                 return true;
             }
         }
         return false;
     }
-    public User loginUser(String username){
-        for (User user : users){
-            if(user != null && user.getUsername().equals(username)){
+
+    public User loginUser(String username) {
+        for (User user : users) {
+            if (user != null && user.getUsername().equals(username)) {
                 return user;
             }
         }
@@ -37,12 +39,50 @@ public class UserService implements InterfaceService{
     }
 
     @Override
-    public void update(UUID id , String name) {
-         }
+    public void update(UUID id, String name) {
+        for (User user : users) {
+            if (user != null && user.getId().equals(id) && !user.getUsername().equals(name)) {
+                user.setUsername(name);
+            }
+        }
+    }
+
+    public boolean isExistUser(UUID id) {
+        for (User user : users) {
+            if (user != null && user.getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getUserIndexByUserId(UUID userId) {
+        for (int i = 0; i < userCount; i++) {
+            if (users[i] != null && users[i].getId().equals(userId)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     @Override
     public boolean delete(UUID id) {
-        return false;
+        if (!isExistUser(id)) {
+            return false;
+        }
+        int indexDelete = getUserIndexByUserId(id);
+
+        if (indexDelete == -1) {
+            return false;
+        }
+
+        for (int i = indexDelete; i < userCount; i++) {
+            if (users[i] != null) {
+                users[i] = users[i + 1];
+            }
+        }
+        userCount--;
+        return true;
     }
 
     @Override
