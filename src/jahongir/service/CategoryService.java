e2b13835804e72446f7401314b07e56e3615edc9
akmal <akmalnajimov70@gmail.com>
2024-03-13 15:02:52 +0500

    Check only Add,List,isExist,validate in category Service

--- src/jahongir/service/CategoryService.java
+++ src/jahongir/service/CategoryService.java
@@ -24,9 +24,10 @@ public class CategoryService extends BaseService {
     @Override
     public boolean add(Object object) {
         Category category = (Category) object;
-
-        if (validate(category)) {}
-
+        if (validate(category)) {
+            categories[indexCategories++] = category;
+            return true;
+        }
         return false;
     }
 
@@ -51,29 +52,39 @@ public class CategoryService extends BaseService {
     @Override
     public boolean update(UUID id, Object object) {
         Category updateCategory = (Category) object;
-
         for (int i = 0; i < indexCategories; i++) {
             if (categories[i].getId().equals(id)) {
                 categories[i].setName(updateCategory.getName());
                 return true;
             }
         }
-
         return false;
     }
 
     @Override
     public Object[] list(UUID id) {
+        if(!isExist(id)){
+            return null;
+        }
         Category[] parentCategories = new Category[100];
         int index = 0;
 
         return parentCategories;
     }
 
-    private boolean validate(Category category) {
-
+    private boolean isExist(UUID name) {
+        for (Category category : categories) {
+            if (category != null && category.getId().equals(name)) {
+                return true;
+            }
+        }
         return false;
     }
 
+    private boolean validate(Category category) {
+        return category != null && category.getName() != null || category.getName().isEmpty() ;
+    }
 }
+
+
 
